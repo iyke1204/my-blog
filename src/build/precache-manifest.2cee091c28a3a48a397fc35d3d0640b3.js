c318caf7baacce25b31f5d306bc997081384125a
Jude I <jude.i@outlook.com>
2020-05-06 11:56:10 +1000

    First commit

---
+++ src/build/precache-manifest.2cee091c28a3a48a397fc35d3d0640b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2772394747e977ac86ebefe425c242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8459538b6e8cd1ceba96",
+    "url": "/static/css/main.ae95ee3c.chunk.css"
+  },
+  {
+    "revision": "873b7fa8022ee5049204",
+    "url": "/static/js/2.c6bc15cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c6bc15cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8459538b6e8cd1ceba96",
+    "url": "/static/js/main.c9151986.chunk.js"
+  },
+  {
+    "revision": "053c8e34507918769709",
+    "url": "/static/js/runtime-main.aecaeffa.js"
+  }
+]);
